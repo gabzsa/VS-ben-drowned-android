5c2bf2ac058c8a796b371ccf7034cffed2228b0c
gabzsa <109770528+gabzsa@users.noreply.github.com>
2023-02-20 11:43:53 -0300

    Opção

--- source/Options.hx
+++ source/Options.hx
@@ -38,6 +38,25 @@ class OptionCategory
 		_options = options;
 	}
 }
+class AndroidControls extends Option
+{
+	public function new()
+	{
+		super();
+	}
+
+	public override function press():Bool
+	{
+		trace("switch");
+		FlxG.switchState(new android.AndroidControlsMenu());
+		return false;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Android Controls";
+	}
+}
 
 class Option
 {
